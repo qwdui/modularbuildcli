f060ddc743107b5858cb3e07c0cc281fc9b8490e
qwdui <gs1yoprasan@hotmail.com>
2024-05-08 04:31:08 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
+function rotate(matrix) {
+  const n = matrix.length;
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    for (let j = i; j < n - i - 1; j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[n - j - 1][i];
+      matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+      matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+      matrix[j][n - i - 1] = temp;
+    }
+  }
+}
