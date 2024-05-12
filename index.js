ee7cda77087397571ee2722a4969ea1733d079a2
qwdui <gs1yoprasan@hotmail.com>
2024-05-12 08:55:38 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-function maxEnvelopes(envelopes) {
-  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
-  const dp = new Array(envelopes.length).fill(1);
-  for (let i = 0; i < envelopes.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (envelopes[i][1] > envelopes[j][1]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
+const bubbleSortOptimized = (arr) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
       }
     }
-  }
-  return Math.max(...dp);
-}
+  } while (swapped);
+  return arr;
+};
