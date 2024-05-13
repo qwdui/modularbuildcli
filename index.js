cd8eeb40eb1fd0b07c06807959d4249ed345a6e0
qwdui <gs1yoprasan@hotmail.com>
2024-05-13 13:54:17 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const bubbleSortOptimized = (arr) => {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        swapped = true;
-      }
-    }
-  } while (swapped);
-  return arr;
-};
+const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n);
